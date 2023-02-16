c7dcde9248664ff0768439e09ba5551c9427b710
seunghyun711 <seunghyun711@gmail.com>
2023-02-16 22:07:29 +0900

    Contrller 클래스 생성

---
+++ src/main/java/com/example/coffee/CoffeeController.java
@@ -0,0 +1,9 @@
+package com.example.coffee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vi/coffees")
+public class CoffeeController {
+}
---
+++ src/main/java/com/example/member/MemberController.java
@@ -0,0 +1,10 @@
+package com.example.member;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vi/members") // 클래스 전체에 사용되는 공통 URL 설정
+public class MemberController {
+
+}
