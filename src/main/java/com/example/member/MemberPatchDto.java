976129de0425ad6f458a0f314c86036628e45800
seunghyun711 <seunghyun711@gmail.com>
2023-02-17 02:38:05 +0900

    Custom Annotation을 사용한 유효성 검증

--- src/main/java/com/example/member/MemberPatchDto.java
+++ src/main/java/com/example/member/MemberPatchDto.java
@@ -1,13 +1,16 @@
 package com.example.member;
 
+import com.example.validator.NotSpace;
+
 import javax.validation.constraints.Pattern;
 
 public class MemberPatchDto { // 회원 정보 수정 시 Request Body를 전달 받을 때 사용되는 클래스
     private long memberId;
 
     // 공백 아닌 문자 1개 이상((공백인 문자 0개 또는 1개)(공백이 아닌 문자 1개 이상)) -> 마지막 맨 바깥 쪽 괄호 조건이 0개 이상(즉, 있어도 되고 없어도 된다)
-    @Pattern(regexp = "^\\S+(\\s?\\S+)*$",
-    message = "회원 이름은 공백이 올 수 없습니다.")
+//    @Pattern(regexp = "^\\S+(\\s?\\S+)*$",
+//    message = "회원 이름은 공백이 올 수 없습니다.")
+    @NotSpace(message = "회원의 이름은 공백이 올 수 없습니다.") // Custom 애너테이션 사용
     private String name;
 
     @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$")
---
+++ src/main/java/com/example/validator/NotSpaceValidator.java
@@ -0,0 +1,20 @@
+package com.example.validator;
+
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class NotSpaceValidator implements ConstraintValidator<NotSpace,String> {
+
+    @Override
+    public void initialize(NotSpace constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return false;
+    }
+}
