690f658b4f474666d48c97bafda2922cf21a8e3f
seunghyun711 <seunghyun711@gmail.com>
2023-02-17 02:22:12 +0900

    Member patch, post에 대한 유효성 검증 적용

--- src/main/java/com/example/member/MemberController.java
+++ src/main/java/com/example/member/MemberController.java
@@ -3,19 +3,24 @@ package com.example.member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/members") // produces 설정 제거됨
+@Validated // @PathVaraible이 추가된 변수에 유효성 검증이 정상적으로 수행되려면 해당 애너테이션을 붙인다.
 public class MemberController {
     // ResponseEntity클래스로 응답 데이터를 래핑하여 http상태를 더 보기 쉽게 리턴할 수 있다.
 
     // 회원 정보 등록
+    // @Valid : dto 클래스에서 유효성 검사가 실행되려면 @Vaild애너테이션을 붙여준다.
     @PostMapping
-    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) { // dto클래스 적용
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) { // dto클래스 적용
         // @RequestParam : 클라이언트 쪽에서 전송하는 요청 데이터를 쿼리 파라미터, 폼 데이터 형식으로 전송하면 이를 서버에서 전달 받을 때 사용하는 애너테이션
         // JSON 문자열을 직접 입력하지 않고, map으로 대체
 
@@ -25,8 +30,8 @@ public class MemberController {
 
     // 회원 정보 수정
     @PatchMapping("/{member-id}")
-    public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
-                                      @RequestBody MemberPatchDto memberPatchDto){
+    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto){
         memberPatchDto.setMemberId(memberId);
         memberPatchDto.setName("hong");
 
--- src/main/java/com/example/member/MemberPostDto.java
+++ src/main/java/com/example/member/MemberPostDto.java
@@ -1,8 +1,20 @@
 package com.example.member;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class MemberPostDto { // 회원 정보 등록 시 Request Body를 받을 때 사용되는 클래스
+    // 유효성 검사 적용
+    @NotBlank // 공백이 올 수 없다.
+    @Email // 유요한 이메일인지 검증, 유효성 검증에 실패하면 내장된 디폴트 에러 메서지가 출력됨.
     private String email;
+
+    @NotBlank(message = "이름은 공백이 아니어야 합니다.")
     private String name;
+
+    @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$" // 정규 표현식 사용
+    ,message = "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되어야 합니다.")
     private String phone;
 
     public String getEmail() {
