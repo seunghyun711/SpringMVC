6b0abf3eb516b52d6da3f23099683f7d0761f13d
seunghyun711 <seunghyun711@gmail.com>
2023-02-17 19:40:22 +0900

    Member클래스(Entity), MemberService클래스 생성

--- src/main/java/com/example/member/Member.java
+++ src/main/java/com/example/member/Member.java
@@ -1,2 +1,19 @@
-package com.example.member;public class Member {
+package com.example.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 구현
+@AllArgsConstructor // 모든 Member클래스의 멤버 변수를 파리미터로 가지는 생성자를 자동으로 구현
+public class Member {
+    private long memberId;
+    private String email;
+    private String name;
+    private String phone;
+
+
 }
