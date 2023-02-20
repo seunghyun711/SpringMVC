2e21cfa70190107b9d7fcca5865cc04efacc3d62
seunghyun711 <seunghyun711@gmail.com>
2023-02-21 00:22:45 +0900

    GlobalExceptionAdvice 수정 및 @RestControllerAdvice vs @ControllerAdvice

--- src/main/java/com/example/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/example/advice/GlobalExceptionAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
@@ -15,23 +16,19 @@ import java.util.stream.Collectors;
 @RestControllerAdvice // Controller클래스에서 발생하는 예외를 맡아서 처리한다.
 public class GlobalExceptionAdvice {
     @ExceptionHandler
-    public ResponseEntity handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
-        final List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
 
-        List<ErrorResponse.FieldError> errors =
-                fieldErrors.stream()
-                        .map(error -> new ErrorResponse.FieldError(
-                                error.getField(),
-                                error.getRejectedValue(),
-                                error.getDefaultMessage()))
-                        .collect(Collectors.toList());
-        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
+        return response;
     }
 
     @ExceptionHandler
-    public ResponseEntity handleConstraintViolationException(ConstraintViolationException e) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e){
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
     }
 
 }
