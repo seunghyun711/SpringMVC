4d62aa8049816459e139acb5ce8879df35a0e83e
seunghyun711 <seunghyun711@gmail.com>
2023-02-28 23:20:03 +0900

    Coffee 엔티티 클래스 필드와 컬럼 간 매핑

--- src/main/java/com/example/coffee/Coffee.java
+++ src/main/java/com/example/coffee/Coffee.java
@@ -5,13 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long coffeeId;
+
+    @Column(nullable = false, length = 50)
     private String engName;
+    @Column(nullable = false, length = 50)
     private String korName;
+    @Column(nullable = false)
     private int price;
+    @Column(nullable = false, length = 3)
+    private String coffeeCode;
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(nullable = false,name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
