70287aac21a3419f3906dc74ddc01e1eb8b56d14
seunghyun711 <seunghyun711@gmail.com>
2023-03-01 13:32:12 +0900

    MemberService 수정

--- src/main/java/com/example/member/MemberService.java
+++ src/main/java/com/example/member/MemberService.java
@@ -1,36 +1,70 @@
 package com.example.member;
 
+import com.example.exception.BusinessLogicException;
+import com.example.exception.ExceptionCode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberService {
+   private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public Member createMember(Member member){
-        Member createdMember = member;
-        return createdMember;
+        // 이메일 검증
+        verifyExistsEmail(member.getEmail());
+        return memberRepository.save(member);
     }
 
     public Member updateMember(Member member){
-        Member updatedMember = member;
-        return updatedMember;
+        // 회원 검증(id)
+        Member findMember = findVerifiedMember(member.getMemberId());
+        Optional.ofNullable(member.getName())
+                .ifPresent(name -> findMember.setName(name));
+        Optional.ofNullable(member.getPhone())
+                .ifPresent(phone -> findMember.setPhone(phone));
+
+        // 회원 정보 업데이트된 시간 추가
+        findMember.setModifiedAt(LocalDateTime.now());
+        return memberRepository.save(findMember);
     }
 
     public Member findMember(long memberId){
-        Member member =
-                new Member("hgd@gmail.com", "홍길동", "010-1234-5678");
-        return member;
+        return findVerifiedMember(memberId);
     }
 
-    public List<Member> findMembers(){
-        List<Member> members = List.of(
-                new Member("hgd@gmail.com", "홍길동", "010-1234-5678"),
-                new Member("hsd@gmail.com", "홍길똥", "010-6788-1678")
-        );
-        return members;
+    public Page<Member> findMembers(int page, int size){
+        return memberRepository.findAll(PageRequest.of(page, size, Sort.by("memberId").descending()));
     }
 
     public void deleteMember(long memberId){
+        Member findMember = findVerifiedMember(memberId);
+        memberRepository.delete(findMember);
+    }
 
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent()) { // 현재 이메일의 회원이 존재하면 예외 처리
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+        }
     }
 }
