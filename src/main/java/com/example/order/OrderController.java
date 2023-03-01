c8a78978a8cb875644e16a061e4c110c2e95a4ad
seunghyun711 <seunghyun711@gmail.com>
2023-03-01 16:34:50 +0900

    OrderMapper, OrderController 수정

--- src/main/java/com/example/order/OrderController.java
+++ src/main/java/com/example/order/OrderController.java
@@ -1,39 +1,74 @@
 package com.example.order;
 
+import com.example.coffee.CoffeeService;
+import com.example.order.mapper.OrderMapper;
+import com.example.response.MultiResponseDto;
+import com.example.response.SingleResponseDto;
+import com.example.utils.UriCreator;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/v1/orders")
+@Validated
 public class OrderController {
-    @PostMapping
-    public ResponseEntity postOrder(@RequestParam("memberId") long memberId,
-                                    @RequestParam("coffeeId") long coffeeId) {
-        Map<String, Long> map = new HashMap<>();
-        map.put("memberId", memberId);
-        map.put("coffeeId", coffeeId);
+    private final static String ORDER_DEFAULT_URL = "/v1/orders";
+    private final OrderService orderService;
+    private final OrderMapper mapper;
+    private final CoffeeService coffeeService;
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public OrderController(OrderService orderService, OrderMapper mapper, CoffeeService coffeeService) {
+        this.orderService = orderService;
+        this.mapper = mapper;
+        this.coffeeService = coffeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity postOrder(@Valid @RequestBody OrderPostDto orderPostDto) {
+        Order order = orderService.createOrder(mapper.orderPostDtoToOrder(orderPostDto));
+        URI location = UriCreator.createUri(ORDER_DEFAULT_URL, order.getOrderId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/{order-id}")
+    public ResponseEntity patchOrder(@PathVariable("order-id") @Positive long orderId,
+                                     @Valid @RequestBody OrderPatchDto orderPatchDto) {
+        orderPatchDto.setOrderId(orderId);
+        Order order = orderService.updateOrder(mapper.orderPatchDtoToOrder(orderPatchDto));
+
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.orderToOrderResponseDto(order)),HttpStatus.OK);
     }
 
     @GetMapping("/{order-id}")
-    public ResponseEntity getOrder(@PathVariable("order-id") long orderId) {
-        System.out.println("# orderId: " + orderId);
-
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity getOrder(@PathVariable("order-id") @Positive long orderId) {
+        Order order = orderService.findOrder(orderId);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.orderToOrderResponseDto(order)), HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getOrders() {
-        System.out.println("# get Orders");
+    public ResponseEntity getOrders(@Positive @RequestParam int page,
+                                    @Positive @RequestParam int size) {
+        Page<Order> pageOrders = orderService.findOrders(page - 1, size);
+        List<Order> orders = pageOrders.getContent();
 
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(mapper.ordersToOrderResponseDtos(orders), pageOrders), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{order-id}")
+    public ResponseEntity cancelOrder(@PathVariable("order-id") @Positive long orderId){
+        orderService.cancelOrder(orderId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/order/mapper/OrderMapper.java
+++ src/main/java/com/example/order/mapper/OrderMapper.java
@@ -1,2 +1,18 @@
-package com.example.order.mapper;public interface OrderMapper {
+package com.example.order.mapper;
+
+import com.example.coffee.Coffee;
+import com.example.order.*;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+    Order orderPostDtoToOrder(OrderPostDto orderPostDto);
+
+    Order orderPatchDtoToOrder(OrderPatchDto orderPatchDto);
+
+    OrderResponseDto orderToOrderResponseDto(Order order);
+
+    List<OrderResponseDto> ordersToOrderResponseDtos(List<Order> orders);
 }
