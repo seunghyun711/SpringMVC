4eb20b3b904ae764bf0a2d546c675d2b3d851e3b
seunghyun711 <seunghyun711@gmail.com>
2023-03-01 12:23:34 +0900

    Exception 코드에 exception message 추가

--- src/main/java/com/example/exception/ExceptionCode.java
+++ src/main/java/com/example/exception/ExceptionCode.java
@@ -3,7 +3,14 @@ package com.example.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    MEMBER_NOT_FOUND(404,"Member Not Found");
+    MEMBER_NOT_FOUND(404,"Member Not Found"),
+    MEMBER_EXISTS(409, "Member exists"),
+    COFFEE_NOT_FOUND(404, "Coffee not found"),
+    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
+    ORDER_NOT_FOUND(404, "Order not found"),
+    CANNOT_CHANGE_ORDER(403, "Order can not change"),
+    NOT_IMPLEMENTATION(501, "Not Implementation"),
+    INVALID_MEMBER_STATUS(400, "Invalid member status");  // TO 추가된 부분
 
     @Getter
     private int status;
