c0b57b3165326c456e869fd51391ea482375b920
seunghyun711 <seunghyun711@gmail.com>
2023-03-09 10:41:14 +0900

    MemberContorller patchMember() 테스트 케이스에 API 스펙 정보 추가

--- src/test/java/com/example/SpringMVC/restdocs/member/MemberControllerRestDocsTest.java
+++ src/test/java/com/example/SpringMVC/restdocs/member/MemberControllerRestDocsTest.java
@@ -5,6 +5,7 @@ import com.example.member.MemberController;
 import com.example.member.MemberDto;
 import com.example.member.MemberService;
 import com.example.member.mapper.MemberMapper;
+import com.example.stamp.Stamp;
 import com.google.gson.Gson;
 import org.apiguardian.api.API;
 import org.junit.jupiter.api.Test;
@@ -99,5 +100,69 @@ public class MemberControllerRestDocsTest {
                                 headerWithName(HttpHeaders.LOCATION).description("Location header, 등록된 리소스의 URI") // HTTP response의 Location header를 의미
                         )
                 ));
+    }
+
+    @Test
+    public void patchMemberTest() throws Exception {
+        // given
+        long memberId = 1L;
+        MemberDto.Patch patch = new MemberDto.Patch(memberId, "hong","010-2222-3333", Member.MemberStatus.MEMBER_QUIT);
+        String content = gson.toJson(patch);
+
+        MemberDto.Response responseDto =
+                new MemberDto.Response(1L,
+                        "hong@com",
+                        "hong",
+                        "010-2222-3333",
+                        Member.MemberStatus.MEMBER_QUIT,
+                        new Stamp());
+
+        given(mapper.memberPatchDtoToMember(Mockito.any(com.example.member.MemberPatchDto.class))).willReturn(new Member());
+        given(memberService.updateMember(Mockito.any(Member.class))).willReturn(new Member());
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(responseDto);
+
+
+        // when
+        ResultActions actions =
+                mockMvc.perform(
+                        patch("/v1/members/{member-id}", memberId)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.memberId").value(patch.getMemberId()))
+                .andExpect(jsonPath("$.name").value(patch.getName()))
+                .andExpect(jsonPath("$.phone").value(patch.getPhone()))
+                .andExpect(jsonPath("$.memberStatus").value(patch.getMemberStatus().getStatus()))
+                .andDo(document("patch-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("member-id").description("회원 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").ignored(),// 회원 식별자는 MemberDto.Patch 클래스에서 request body에 매핑되지 않는 정보이므로 ignored()로 API 스펙 정보에서 제외
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름").optional(),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("폰 번호").optional(),
+                                        fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("회원상태").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        //fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"), // JSON 포멧으로 표현된 프로퍼티의 값이 객체임을 의미
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("폰 번호"),
+                                        fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("회원 상태 : 활동중 / 휴면 상태 / 탈퇴 상태"),
+                                        fieldWithPath("stamp").type(JsonFieldType.NUMBER).description("스탬프 개수")
+                                )
+                        )
+                        ));
     }
 }
